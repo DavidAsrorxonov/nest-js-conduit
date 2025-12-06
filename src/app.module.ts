c7c900db9738385297dd18507d19f2d1a2c56d0c
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-12-06 10:14:56 +0900

    feat(config): add @nestjs/config for environment variable management and create ormconfig for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    TagModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
