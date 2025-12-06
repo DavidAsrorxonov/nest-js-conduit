5c2eccc7ced6504bf235870277862f2aa035bbf9
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-12-06 20:00:46 +0900

    feat(user): implement UserService and update UserController for user creation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
   createUser() {
-    return 'User created';
+    return this.userService.createUser();
   }
 }
