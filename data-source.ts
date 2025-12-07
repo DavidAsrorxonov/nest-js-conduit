a9171ea0df0abb4d5e2f6fbea380d9e9ef538111
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-12-07 14:45:55 +0900

    feat(database): add TypeORM data source configuration and dotenv integration

---
+++ data-source.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/src/**/*.entity{.ts, .js}'],
+  migrationsTableName: 'migrations',
+  migrations: [__dirname + 'src/migrations/**/*.{ts,js}'],
+});
